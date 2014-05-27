190da6524a732a6791c10f96ac398aec8b7922b3
ericalthatcher <ericalthatcher@gmail.com>
2014-05-27 11:46:26 -0700

    switched from nytimes api to sunlight foundation api

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
 var title_element,
     title_render,
     element_ids = ['ushd1', 'ushd2', 'ushd3', 'ushd4', 'ushd5'],
-    district_numbers = ['1', '2', '3', '4', '5'],
-    apiKey = 'api-key=6a4c73c2adc542140983b625c9dcf477:18:68003148',
-    baseUrl = 'https://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/house/OR/';
+    apiKey = 'apikey=6e58b9075b9f4244aea471ee0e066e19',
+    chamberURL = 'chamber=house&state=OR&',
+    baseUrl = 'https://congress.api.sunlightfoundation.com/legislators?';
 
-  for (var i = 0, total_districts = district_numbers.length; i < total_districts; i++) {
+  for (var i = 0, total_elements = element_ids.length; i < total_elements; i++) {
       (function (i) {
-        var myUrl = baseUrl + district_numbers[i] + '/current.json?' + apiKey;
+        var myUrl = baseUrl + chamberURL + apiKey;
 
         $.ajax({
           url: myUrl,
@@ -19,7 +19,7 @@ var title_element,
           success: function (data) {
             console.log('\nSUCCESS: ' + myUrl);
             title_element = document.getElementById(element_ids[i]);        
-            title_render = document.createTextNode(data.results[0].name + ' - District ' + data.results[0].district );
+            title_render = document.createTextNode(data.results[i].title + '. ' + data.results[i].first_name + ' ' + data.results[i].last_name + ' - District ' + data.results[i].district );
             title_element.appendChild(title_render);
           },
 
